7b34ca5160af2e0109e4f314548d5ec9fc736a04
Gabriel Costa <129251793+gabrielcostaaa@users.noreply.github.com>
2024-06-03 11:43:03 -0300

    criação listagem tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 import Header from './componets/Header'
+import UserList from './componets/UserList'
 
 const api = axios.create({
   baseURL: 'http://localhost:3333'
@@ -9,16 +10,22 @@ const api = axios.create({
 
 function App() {
   const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
 
+useEffect(() => {
   api.get('/users').then( (response) => {
-    console.log(response)
+    setUsers(response.data.users)
+  }).catch(error => {
+    console.error("deu um problema", error)
   })
+}, [])
 
-  return (
-    <div className="">
-      <Header/>
-    </div>
-  )
+return (
+  <div className="flex flex-col items-center justify-center min-h-screen p-4">
+    <Header />
+    <UserList users={users} />
+  </div>
+);
 }
 
 export default App
